f52923259f8b3cdb06e6a92bc4094ba615088aea
vhegde <vhegde@uw.edu>
2022-06-25 21:16:18 -0700

    Updated max_nums.js

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,13 @@
 // start coding your function here!
+function find_max(arr){
+  let max = arr[0];
+  for (let i=1; i <arr.length; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+//console.log(find_max([1, 2, 3, 3, -1]));
+exports.find_max = find_max;
